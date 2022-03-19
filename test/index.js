70b6a103d53400197792cf6b3404b4ad20443556
ds1242 <david.shaw1242@gmail.com>
2022-03-19 09:18:32 -0600

    added mocha, renamed dirctory to simplifiy test command

--- test/index.js
+++ test/index.js
@@ -5,12 +5,42 @@ require('chromedriver');
 
 let driver = new Builder().forBrowser('chrome').build();
 
-async function login() {  
+// async function login() {  
 
+//   // Log into tech blog to add a post
+//   let login =  await driver.wait(until.elementLocated(By.id('login')))
+//   login.click();
+
+//   let email = await driver.wait(until.elementLocated(By.id('email-login')));
+//   let password = await driver.wait(until.elementLocated(By.id('password-login')));
+//   let loginClick = await driver.wait(until.elementLocated(By.className('btn-secondary')));
+//   let home = await driver.wait(until.elementLocated(By.id('home')));
+//   await email.sendKeys('tt25@tt25.com');
+//   await password.sendKeys('password');
+//   await loginClick.click();
+//   await home.click();
+
+// }
+
+
+// async function createPost() {
+  
+  
+
+// }
+
+// describe block
+
+describe('Login, check post count, add post', function() {
+  // it block to check for number of posts
+  it('successfully logins and checks the number of posts on the main page', async function() {
+    // Navigate to Url
+  await driver.get('https://guarded-cliffs-83768.herokuapp.com/');
+  
   // Log into tech blog to add a post
   let login =  await driver.wait(until.elementLocated(By.id('login')))
   login.click();
-
+    // find email, password, login, and home buttons
   let email = await driver.wait(until.elementLocated(By.id('email-login')));
   let password = await driver.wait(until.elementLocated(By.id('password-login')));
   let loginClick = await driver.wait(until.elementLocated(By.className('btn-secondary')));
@@ -19,17 +49,22 @@ async function login() {
   await password.sendKeys('password');
   await loginClick.click();
   await home.click();
-
-}
-
-
-async function createPost() {
   
+  let postCount = (await driver.findElements(By.className('post'))).length;
   
 
-}
+  
 
-async function example() {
+  // assert using node assertion  
+  assert.strictEqual(postCount, 9);
+  await driver.quit();
+  });
+
+});
+
+
+
+async function loginAndCountPosts() {
 
   // Navigate to Url
   await driver.get('https://guarded-cliffs-83768.herokuapp.com/');
@@ -65,23 +100,17 @@ async function example() {
   // createButton.click();
   
   let postCount = (await driver.findElements(By.className('post'))).length;
-  console.log('post count is ' + postCount);
-
+  
 
   
 
-  // assert
-  // let allHeaderText = await driver.findElements(By.className("post")).getText().then(function(values) {
-  //   return values;
-  // });
-  // console.log(allHeaderText)
-  
-  // assert.strictEqual(allHeaderText, 'New Post With Selenium');
+  // assert using node assertion  
+  assert.strictEqual(postCount, 9);
 
   // await driver.quit();
   
     
   };
 
-example();
+
 
