c6f0ed1a168e565b8293feb46589a19b3cd04b06
ds1242 <david.shaw1242@gmail.com>
2022-03-20 13:18:39 -0600

    used link text to pull up search results

--- test/search.js
+++ test/search.js
@@ -7,7 +7,7 @@ let driver = new Builder().forBrowser('chrome').build();
 
 describe('Search for a trail', function() {
 
-    it('Search for a trail and return results', function() {
+    it('Search for a trail and return results', async function() {
         // Navigate to Url
         await driver.get('https://www.trailforks.com/');
         // find search box and button
@@ -17,6 +17,18 @@ describe('Search for a trail', function() {
         await searchBox.sendKeys('Bobsled');
         await searchButton.click();
         // check url to see if results are generated
-        
+        let searchPageUrl = await driver.getCurrentUrl();
+
+        // assert using node assertion
+        assert.equal(searchPageUrl, 'https://www.trailforks.com/search/?search=Bobsled');
+
+        await driver.quit();
+    });
+
+    it('Pull up trail details and information page', async function() {
+        await driver.get('https://www.trailforks.com/search/?search=Bobsled');
+
+        let searchResult = await driver.wait(until.elementLocated(By.linkText('Bobsled')));
+        console.log(searchResult.length);
     })
 })
